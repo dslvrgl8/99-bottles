57cad22266a3338c86c0a3bd558e6611227a3250
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-03-21 11:09:24 -0700

    Take one down done

--- server.js
+++ server.js
@@ -2,25 +2,27 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send(`Bottles of Beer on the wall.
+   let beers = 99
+    res.send(` 99 Bottles of Beer on the wall.
         Take one down, pass it around. <a href=/${beers}> 
         ${beers-1} Bottles of Beer on the wall.`)
 }),
 
 //new number is id -1
 app.get('/:id', (req, res) => {
-    const beers = req.params.id
+    let beers = req.params.id
     if(beers >= 2) {
         res.send(`${beers} Bottles of Beer on the wall.
-        Take one down, pass it around. <a href=/${beers}> 
-        ${beers-1} Bottles of Beer on the wall.`) 
+        Take one down, pass it around. <a href=/${beers-1}> 
+        ${beers-1} Bottles of Beer on the wall.`);
 
-    } else if(req.params.id == 1) {
+    } else if(beers == 1) {
 
-        res.send('No more beer. Sorry.')
+        res.send(`This last bottle is for me, No more beer. Sorry. <a href=/${beers-1}> 
+        ${beers-1} Bottles of nothing on the wall.`)
 
     } else {
-
+        res.send("We're out")
     }
 }),
 
